4c646133ea80c3ecaf1eecd78711b10fe553465d
imgrizzly <imgrizzly@foxmail.com>
2019-10-09 11:09:52 +0800

    增加目录创建

--- app/src/main/java/com/example/freezemotionanimation/MainActivity.java
+++ app/src/main/java/com/example/freezemotionanimation/MainActivity.java
@@ -9,21 +9,48 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
+
+import java.io.File;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private String mFilePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFilePath = getFilesDir().getPath();
     }
 
 
+    public static String createDir(String dirPath){
+        //因为文件夹可能有多层，比如:  a/b/c/ff.txt  需要先创建a文件夹，然后b文件夹然后...
+        try{
+            File file=new File(dirPath);
+            if(file.getParentFile().exists()){
+                Log.i(TAG, "createDir: " + file.getAbsolutePath());
+                file.mkdir();
+                return file.getAbsolutePath();
+            }
+            else {
+                createDir(file.getParentFile().getAbsolutePath());
+                Log.i(TAG, "createDir: "+ file.getAbsolutePath());
+                file.mkdir();
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return dirPath;
+    }
 
 
     public void startCap(View view){
+        createDir(mFilePath + "/data/" + "test");
         startActivity(new Intent(this, Main2Activity.class));
     }
 
